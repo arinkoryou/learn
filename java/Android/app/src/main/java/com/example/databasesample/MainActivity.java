640393faf3048770237a5cf0d22b816951db6237
Ryouhei <ryou.arinko@gmail.com>
2020-01-25 16:05:35 +0900

    DB登録処理サンプル作成

--- java/Android/app/src/main/java/com/example/databasesample/MainActivity.java
+++ java/Android/app/src/main/java/com/example/databasesample/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.databasesample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -62,10 +64,30 @@ public class MainActivity extends AppCompatActivity {
         SQLiteDatabase db = helper.getWritableDatabase();
 
         try{
+            // まず、リストで選択されたカクテルのメモデータを削除。その後、インサートを行う。
+            // 削除用SQL文字列を用意。
+            String sqlDelete = "DELETE FROM cocktailmemo WHERE _id = ?";
+            // SQL文字列をもとにプリペアドステートメントを取得。
+            SQLiteStatement stmt = db.compileStatement(sqlDelete);
+            // 変数のバインド。
+            stmt.bindLong(1, _cocktailID);
+            // 削除SQLの実行。
+            stmt.executeUpdateDelete();
 
+            // インサート用SQL文字列用意。
+            String sqlInsert = "INSERT INTO cocktailmemo (_id, name, note) VALUES(?, ?, ?)";
+            // SQL文字列をもとにプリペアドステートメントを取得。
+            stmt = db.compileStatement(sqlInsert);
+            // 変数のバインド。
+            stmt.bindLong(1, _cocktailID);
+            stmt.bindString(2, _cocktailName);
+            stmt.bindString(3, note);
+            // インサートSQLの実行。
+            stmt.executeInsert();
         }
         finally {
-
+            // データベース接続オブジェクトの開放。
+            db.close();
         }
         // カクテル名を「未選択」に変更。
         _tvCocktailName.setText(getString(R.string.tv_name));
@@ -90,6 +112,33 @@ public class MainActivity extends AppCompatActivity {
             _tvCocktailName.setText(_cocktailName);
             // [保存] ボタンをタップできるように設定。
             _btnSave.setEnabled(true);
+
+            // データベースヘルパーオブジェクトを作成。
+            DatabaseHelper helper = new DatabaseHelper(MainActivity.this);
+            // データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得。
+            SQLiteDatabase db = helper.getWritableDatabase();
+            try{
+                // 主キーによる検索SQL文字列を用意。
+                String sql = "SELECT * FROM cocktailmemo WHERE _id = " + _cocktailID;
+                // SQL実行。
+                Cursor cursor = db.rawQuery(sql, null);
+                // データベースから取得した値を格納する変数を用意。データがなかった時のための初期値も用意。
+                String note = "";
+                // SQL実行の戻り値であるカーソルオブジェクトをループさせてデータベース内のデータを取得。
+                while(cursor.moveToNext()) {
+                    // カラムのインデックス値を取得。
+                    int idxNote = cursor.getColumnIndex("note");
+                    // カラムのインデックス値をもとに実際のデータを取得。
+                    note = cursor.getString(idxNote);
+                }
+                // 感想のEditTextの各画面部品を取得しデータベースの値を反映。
+                EditText etNote = findViewById(R.id.etNote);
+                etNote.setText(note);
+            }
+            finally {
+                // データベースオブジェクトの開放.
+                db.close();
+            }
         }
     }
 }
